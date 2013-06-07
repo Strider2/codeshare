4d4914a641a2c5ce3c45fd02c2afc16c246bfa7f
Daniel Counahan <boeing7474@gmail.com>
2013-06-07 23:00:45 +0100

    Changed the code to allow different prefixes

--- core/common/CodeShareData.class.php
+++ core/common/CodeShareData.class.php
@@ -4,42 +4,42 @@ class CodeShareData extends CodonData
 {
     public function get_codeshare()
     {
-        $query = "'SELECT * FROM phpvms_codeshares'
-                    ORDER BY id ASC";
+        $query = "'SELECT * FROM ".TABLE_PREFIX."codeshares'
+                    ORDER BY schedid ASC";
 
         return DB::get_results($query);
 		
     }
  	public function get_upcoming_codeshares()
     {
-        $query = "SELECT * FROM phpvms_codeshares
+        $query = "SELECT * FROM ".TABLE_PREFIX."codeshares
                 ORDER BY schedid ASC";
 
         return DB::get_results($query);
     }
     public function get_codeshares($id)
     {
-        $query = "SELECT * FROM phpvms_codeshares WHERE id='$id'";
+        $query = "SELECT * FROM ".TABLE_PREFIX."codeshares WHERE id='$id'";
 
         return DB::get_row($query);
     }
    public function get_past_codeshare()
     {
-        $query = "SELECT * FROM phpvms_codeshares
+        $query = "SELECT * FROM ".TABLE_PREFIX."codeshare
                 ORDER BY schedid DESC";
 
         return DB::get_results($query);
     }
     public function save_new_codeshare($schedid, $airline)
     {
-        $query = "INSERT INTO phpvms_codeshares (schedid, airline)
+        $query = "INSERT INTO ".TABLE_PREFIX."codeshares (schedid, airline)
                 VALUES ('$schedid', '$airline')";
 
         DB::query($query);
     }
      public function save_edit_codeshare($schedid, $airline, $id)
     {
-        $query = "UPDATE phpvms_codeshares SET
+        $query = "UPDATE ".TABLE_PREFIX."codeshares SET
          schedid='$schedid',
          airline='$airline'
          WHERE id='$id'";
@@ -49,7 +49,7 @@ class CodeShareData extends CodonData
     
     public function delete_codeshare($id)
     {
-        $query = "DELETE FROM phpvms_codeshares
+        $query = "DELETE FROM ".TABLE_PREFIX."codeshares
                     WHERE id='$id'";
 
         DB::query($query);
