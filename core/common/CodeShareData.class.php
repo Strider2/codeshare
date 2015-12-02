0b22374261c60e2062cf9cd9646d6a9fe4af7f80
Daniel Counahan <boeing7474@gmail.com>
2015-12-02 22:13:40 +0000

    Removed code that was put in by github

--- core/common/CodeShareData.class.php
+++ core/common/CodeShareData.class.php
@@ -1,5 +1,4 @@
 <?php
-
 class CodeShareData extends CodonData
 {
     public static function get_codeshare()
@@ -27,38 +26,5 @@ class CodeShareData extends CodonData
 
         return DB::get_results($query);
     }
-<<<<<<< HEAD
-    public static function save_new_codeshare($schedid, $airline)
-=======
-    public function save_new_codeshare($schedid, $airline, $image)
->>>>>>> origin/master
-    {
-        $query = "INSERT INTO phpvms_codeshares (schedid, airline, image)
-                VALUES ('$schedid', '$airline', '$image')";
-
-        DB::query($query);
-    }
-<<<<<<< HEAD
-     public static function save_edit_codeshare($schedid, $airline, $id)
-=======
-     public function save_edit_codeshare($schedid, $airline, $image, $id)
->>>>>>> origin/master
-    {
-        $query = "UPDATE phpvms_codeshares SET
-         schedid='$schedid',
-         airline='$airline',
-         image= '$image',
-         WHERE id='$id'";
-
-        DB::query($query);
-    }
-    
-    public static function delete_codeshare($id)
-    {
-        $query = "DELETE FROM phpvms_codeshares
-                    WHERE id='$id'";
-
-        DB::query($query);
-    }
-   
 }
+?>
