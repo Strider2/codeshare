62e86ce8c36d32587f444c7023b7c47f48a8e808
Daniel Counahan <boeing7474@gmail.com>
2015-12-02 22:11:08 +0000

    Fixed the strict error
    The old version of this will show errors after php 5.5.x This will fix
    that problem

--- core/common/CodeShareData.class.php
+++ core/common/CodeShareData.class.php
@@ -2,39 +2,39 @@
 
 class CodeShareData extends CodonData
 {
-    public function get_codeshare()
+    public static function get_codeshare()
     { 
 		return DB::get_results("SELECT * FROM ".TABLE_PREFIX."codeshares ORDER BY airline ASC");
 		
     }
- 	public function get_upcoming_codeshares()
+ 	public static function get_upcoming_codeshares()
     {
         $query = "SELECT * FROM phpvms_codeshares
                 ORDER BY schedid ASC";
 
         return DB::get_results($query);
     }
-    public function get_codeshares($id)
+    public static function get_codeshares($id)
     {
         $query = "SELECT * FROM phpvms_codeshares WHERE id='$id'";
 
         return DB::get_row($query);
     }
-   public function get_past_codeshare()
+   public static function get_past_codeshare()
     {
         $query = "SELECT * FROM phpvms_codeshare
                 ORDER BY schedid DESC";
 
         return DB::get_results($query);
     }
-    public function save_new_codeshare($schedid, $airline)
+    public static function save_new_codeshare($schedid, $airline)
     {
         $query = "INSERT INTO phpvms_codeshares (schedid, airline)
                 VALUES ('$schedid', '$airline')";
 
         DB::query($query);
     }
-     public function save_edit_codeshare($schedid, $airline, $id)
+     public static function save_edit_codeshare($schedid, $airline, $id)
     {
         $query = "UPDATE phpvms_codeshares SET
          schedid='$schedid',
@@ -44,7 +44,7 @@ class CodeShareData extends CodonData
         DB::query($query);
     }
     
-    public function delete_codeshare($id)
+    public static function delete_codeshare($id)
     {
         $query = "DELETE FROM phpvms_codeshares
                     WHERE id='$id'";
