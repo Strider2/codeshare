c9567e23fa94997971e5cb9c7c9a767d2b1af2ad
Daniel Counahan <boeing7474@gmail.com>
2021-02-20 17:43:22 +0000

    Fixing a small bug

--- admin/templates/codeshare/airline/airlines_airline.php
+++ admin/templates/codeshare/airline/airlines_airline.php
@@ -8,6 +8,6 @@ echo '<h4>Airline Name:'.$airlines->name.'</h4><hr />';
 echo 'Airline logo: <b><img src="'. SITE_URL .'/lib/skins/SKIN_NAME_HERE/images/logos/'.$airlines->code.'.png" alt="'.$airlines->name.'" /></b><hr />';
 
 echo '</b><hr />';
-echo '<a href="'.SITE_URL.'/admin/index.php/codeshare_admin/edit_codeshare_airline?code='.$airlines->code.'"><b>Edit Airline</b></a><br /><hr />';
+echo '<a href="'.SITE_URL.'/admin/index.php/codeshare_admin/edit_codeshare_airline?id='.$airlines->id.'"><b>Edit Airline</b></a><br /><hr />';
 echo '<a href="'.SITE_URL.'/admin/index.php/codeshare_admin/delete_codeshare_airline?id='.$airlines->id.'"><b>Delete Airline</b></a> - This will delete the Airline from the datbase permanently!<br /><hr />';
 ?>
